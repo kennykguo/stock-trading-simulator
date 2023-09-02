a6325c1f2c00f1e43fa77a08290a1c0584da73e3
Kenny Guo <kennykguo@gmail.com>
2023-09-02 09:53:15 -0400

    version 3

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
 
+#version3
+
 # Configure application
 app = Flask(__name__)
 
